1896b82b2c7cebf5dc6336c684b88b6a29142f35
Musa C. Yurt <yurtmusa@gmail.com>
2024-03-21 00:09:51 +0000

    Copilot sixth commit

--- comments.js
+++ comments.js
@@ -1,37 +1,43 @@
 // Create web server
-// Node.js HTTP server to handle incoming requests
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+// Create a route to get all comments
+// Create a route to get a single comment
+// Create a route to add a comment
+// Create a route to update a comment
+// Create a route to delete a comment
 
-// Create server
-http.createServer( function (request, response) {  
-   // Parse the request containing file name
-   var pathname = url.parse(request.url).pathname;
-   
-   // Print the name of the file for which request is made.
-   console.log("Request for " + pathname + " received.");
-   
-   // Read the requested file content from file system
-   fs.readFile(pathname.substr(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html'});
-      } else {  
-         //Page found   
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});  
-         
-         // Write the content of the file to response body
-         response.write(data.toString());       
-      }
-      // Send the response body 
-      response.end();
-   });   
-}).listen(8081);
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const comments = require('./comments');
+const PORT = 3000;
 
-// Console will print the message
-console.log('Server running at http://localhost:8081/');
+app.use(bodyParser.json());
+
+app.get('/comments', (req, res) => {
+  res.json(comments.getAllComments());
+});
+
+app.get('/comments/:id', (req, res) => {
+  const id = req.params.id;
+  res.json(comments.getCommentById(id));
+});
+
+app.post('/comments', (req, res) => {
+  const newComment = req.body;
+  res.json(comments.addComment(newComment));
+});
+
+app.put('/comments/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedComment = req.body;
+  res.json(comments.updateComment(id, updatedComment));
+});
+
+app.delete('/comments/:id', (req, res) => {
+  const id = req.params.id;
+  res.json(comments.deleteComment(id));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
